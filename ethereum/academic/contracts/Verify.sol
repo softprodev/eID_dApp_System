53e4d317d1c918d7824952b1419884f5954a06eb
SoftProDev <softprodev15@gmail.com>
2020-07-28 20:37:38 +0800

    after demo

--- ethereum/academic/contracts/Verify.sol
+++ ethereum/academic/contracts/Verify.sol
@@ -1,31 +1,67 @@
 pragma solidity >= 0.6.0 < 0.7.0;
 pragma experimental ABIEncoderV2;
+import "./Registry.sol";
+import "./Entity.sol";
+import "./CreateEntity.sol";
 
 contract Verify {
+    struct School {
+        string name;
+        address schoolAddr;
+    }
+    
+    School[] public schools;
     address public MinistryofEducation;
-    address[] public schools;
     string[] public certHashOnChain;
     string public certHash;
     mapping(address => bool) isSchool;
     mapping(string => bool) isOnChain;
+    address CEaddress;
     
     constructor() public {
         MinistryofEducation = msg.sender;
     }
-    
-    modifier restricted() {
-        require(isSchool[msg.sender] || msg.sender == MinistryofEducation);
+
+    modifier restricted_school() {
+        CreateEntity search = CreateEntity(address(0x950BD33F71A716B0a6161eBC09Cd89F446698abf));
+        address toSearch = search.searchEntity(msg.sender);
+        require(isSchool[toSearch]);
         _;
     }
     
-    function newSchool(address school) public restricted {
-        require(!isSchool[school]);
-        schools.push(school);
-        isSchool[school] = true;
+    modifier restricted_ministry() {
+        require(msg.sender == MinistryofEducation);
+        _;
     }
     
-    function upload(string memory hashValue) public restricted {
+    function changeAddress(address addr) public {
+        CEaddress = addr;
+    }
+
+    function addNewSchool(address schoolAddr, string memory schoolName) public restricted_ministry {
+        require(!isSchool[schoolAddr]);
+
+        Entity entitySchool = Entity(schoolAddr);
+        address addrRegistry = entitySchool.registry();
+        Registry registrySchool = Registry(addrRegistry);
+        registrySchool.writeFromOtherEntity("isSchool");
+        
+        School memory newSchool = School({
+            name: schoolName,
+            schoolAddr: schoolAddr
+        });
+        schools.push(newSchool);
+        isSchool[schoolAddr] = true;
+    }
+    
+    function upload(string memory hashValue, address student) public restricted_school {
         require(!isOnChain[hashValue]);
+
+        Entity entityStudent = Entity(student);
+        address addrRegistry = entityStudent.registry();
+        Registry registryStudent = Registry(addrRegistry);
+        registryStudent.writeFromOtherEntity("CertisIssued");
+
         certHashOnChain.push(hashValue);
         isOnChain[hashValue] = true;
     }
@@ -36,8 +72,19 @@ contract Verify {
     }
     
     function legality(address school) public view {
-        address addr = school;
-        require(isSchool[addr]);
+        Entity entitySchool = Entity(school);
+        address addrRegistry = entitySchool.registry();
+        Registry registrySchool = Registry(addrRegistry);
+        string memory text = registrySchool.dataField(address(this),0);
+        require(keccak256(abi.encodePacked(text)) == keccak256(abi.encodePacked("isSchool")));
+    }
+
+    function existence(address student) public view {
+        Entity entityStudent = Entity(student);
+        address addrRegistry = entityStudent.registry();
+        Registry registryStudent = Registry(addrRegistry);
+        string memory text = registryStudent.dataField(address(this),0);
+        require(keccak256(abi.encodePacked(text)) == keccak256(abi.encodePacked("CertisIssued")));
     }
     
     function getSchoolsCount() public view returns (uint256) {
@@ -47,13 +94,4 @@ contract Verify {
     function getDeployedCerts() public view returns (string[] memory) {
         return certHashOnChain;
     }
-    
-    /*function notrevoked() public {
-        
-    }
-    
-    function existence() public {
-        
-    }*/
-    
 }
--- ethereum/academic/verify.js
+++ ethereum/academic/verify.js
@@ -4,7 +4,7 @@ import Verify from './build/Verify.json';
 const instance = new web3.eth.Contract(
     //JSON.parse(Verify.interface),
     Verify.abi,
-    '0xF3737C8bA78960A43624298585519D1637652aD5'
+    '0xc7FfD002bf9857831c2B9ed6E15d53a293c7B630'
 );
 
 export default instance;
