a7b15d0483b6b9747cbe55c87e90478de043b7e4
SoftProDev <softprodev15@gmail.com>
2020-07-28 14:53:07 +0800

    prepare merge

--- routes.js
+++ routes.js
@@ -12,8 +12,6 @@ routes.add('/index', '/')
       .add('/Academic/certificates/:address/transcript', '/Academic/certificates/transcript')
       .add('/Academic/school/index', '/Academic/school/index')
       .add('/Academic/school/students', '/Academic/school/students')
-      .add('/Academic/school/schoolList', '/Academic/school/schoolList')
-      .add('/Eid/List','/Eid/List')
-      .add('/Eid/CreateEntity', '/Eid/CreateEntity')
-      .add('/Eid/control', '/Eid/control');
+      .add('/Academic/ministry/index', '/Academic/ministry/index')
+      .add('/Academic/ministry/schoolList', '/Academic/ministry/schoolList');
 module.exports = routes;
