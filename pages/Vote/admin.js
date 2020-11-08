4288e48d4e038260364f05257acc7adb2e8ca3ff
SoftProDev <softprodev15@gmail.com>
2020-11-08 22:11:33 +0800

    record

--- pages/Vote/admin.js
+++ pages/Vote/admin.js
@@ -28,14 +28,12 @@ class Admin extends Component {
           option:'',
           key:'',
           value:'',
-          type:0,
+          type:'',
           loading : false,
           loading2: false,
           loading3 : false,
           search:'',
-          errorMessage:'',
-          exponent:3,
-          M:2
+          errorMessage:''
         };
       //console.log(props.query.address);//擷取這個網址的url那part的address(from routes??) 
       console.log(props.vst); 
@@ -81,7 +79,7 @@ class Admin extends Component {
 
         const Vote = vote(this.props.address);
 
-        const {vote_start_date,vote_end_date,requirement_description,write_entity_address,question,exponent,M} = this.state;
+        const {vote_start_date,vote_end_date,requirement_description,write_entity_address,question} = this.state;
         
         this.setState({loading:true,errorMessage:''});
         
@@ -92,9 +90,7 @@ class Admin extends Component {
                 vote_start_date.getTime(),
                 vote_end_date.getTime(),
                 requirement_description,
-                write_entity_address,
-                exponent,
-                M
+                write_entity_address
             ).send(
                 {from:accounts[0]}
             );
@@ -134,22 +130,18 @@ class Admin extends Component {
         event.preventDefault();
         const Vote = vote(this.props.address);
         const {key, value,type} = this.state;
-
-        console.log(this.state.type)
-        let adjusted_value = (type == 0)?value:value.getTime().toString();
-        console.log(adjusted_value);
         this.setState({loading3:true});
         
         try{
             const accounts = await web3.eth.getAccounts();
             await Vote.methods.set_up_requirement(
                 key,
-                adjusted_value,
+                value,
                 type
             ).send(
                 {from:accounts[0]}
             );
-            alert("Setting Requirement Successfully");
+            alert("Setting Option Successfully");
             Router.pushRoute(`/Vote/admin/${this.props.mb_addr}/${this.props.address}`);
         }catch(err){
             alert(err);
@@ -232,34 +224,7 @@ class Admin extends Component {
                 </Form.Group>
                     
             </Form.Row>
-            <Form.Row>
-            <Form.Group controlId="exponent">
-                <Form.Label>exponent</Form.Label> 
-                <Form.Control type="text" placeholder="Enter a number for tally" 
-                    value={this.state.exponent} 
-                    onChange = {event => this.setState({exponent:event.target.value})} 
-                />
-                </Form.Group>
-                    
-            </Form.Row>
-            <Form.Row>
-            <Form.Group controlId="M">
-                <Form.Label>M</Form.Label>
-                <Form.Control type="text" placeholder="Enter a number for tally" 
-                    value={this.state.M} 
-                    onChange = {event => this.setState({M:event.target.value})} 
-                />
-                </Form.Group>
-                    
-            </Form.Row>
-            <Form.Row>
-                <Form.Label>
-                    Result(adjust exponent and M to fit your Target voter quantity) : 
-                </Form.Label>
-            </Form.Row>
-            <Form.Row>
-                <Form.Label>{"at most "+Math.pow(this.state.exponent,this.state.M)+" people can join this vote"}</Form.Label>
-            </Form.Row>
+
             <Button variant="primary" type="submit">
             {(this.state.loading)?
                   <>
@@ -291,19 +256,16 @@ class Admin extends Component {
                                 <td><h5>type</h5></td>
                             </tr>
                         </thead>
+                                {this.props.requirements.map((requirement, index) =>
                                 <tbody style={{width: '200px'}}>
-                                    {this.props.requirements.map((requirement, index) =>
-                                <tr key={index}> 
+                                <tr> 
                                     <td>{index+1}</td>
                                     <td>{requirement[0]}</td>
                                     <td>{requirement[1]}</td>
-                                    <td>{(requirement[2]== 0)?"=":
-                                         (requirement[2]== 1)?">":
-                                         (requirement[2]== 2)?"<":"error"}</td>
+                                    <td>{requirement[2]}</td>
                                 </tr>
-                                
-                                )}
                                 </tbody>
+                                )}
                         
                     </Table>
                     <Form.Label>vote requirement key</Form.Label>
@@ -312,33 +274,15 @@ class Admin extends Component {
                         onChange = {event => this.setState({key:event.target.value})}
                     />
                     <Form.Label>vote requirement value</Form.Label>
-                    {(this.state.type == 0)?<>
                     <Form.Control type="text" placeholder="requirement value" 
                         value={this.state.value} 
                         onChange = {event => this.setState({value:event.target.value})}
-                    /></>:<>
-                    <br/>
-                    <Form.Label>
-                        <DateTime value={this.state.value} 
-                                onChange={date => {this.setState({value: date.toDate()});}}></DateTime>
-                
-                    </Form.Label>
-                    <br/>
-                    {(this.state.value)?this.state.value.getTime():""}
-                    <br />
-                    </>
-                    }
-                    
+                    />
                     <Form.Label>vote requirement type</Form.Label>
-                    {/* <Form.Control type="text" placeholder="requirement type" 
+                    <Form.Control type="text" placeholder="requirement type" 
                         value={this.state.type} 
                         onChange = {event => this.setState({type:event.target.value})}
-                    /> */}
-                    <Form.Control as="select" size="lg"  onChange = {event => {this.setState({type:event.target.value,value:""});this.forceUpdate();console.log(this.state.type);}} style={{marginTop : "2%"}}>
-                        <option value={0} key={0}>判斷相等 =</option>
-                        <option value={1} key={1}>判斷大於{">"}</option>
-                        <option value={2} key={2}>判斷小於{"<"}</option>
-                    </Form.Control>
+                    />
                 </Form.Group>
             </Form.Row>
             <Button variant="primary" type="submit" style = {{width :'230px',margin:"2%",marginTop : "3%"}} >
@@ -372,16 +316,15 @@ class Admin extends Component {
                                 <td><h5>current option</h5></td>
                             </tr>
                         </thead>
-                            <tbody style={{width: '200px'}}>
+                        
                                 {this.props.options.map((option, index) =>
-                                
-                                <tr key={index}> 
+                                <tbody style={{width: '200px'}}>
+                                <tr> 
                                     <td>{index+1}</td>
                                     <td>{option}</td>
                                 </tr>
-                                
+                                </tbody>
                                 )}
-                            </tbody>
                         
                     </Table>
                     
